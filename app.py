4008b98f031ea5b77426af26f8497b2af591cf45
Eyimofe <eyimofe36@gmail.com>
2023-08-25 22:11:05 +0100

    added requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -32,6 +32,11 @@ jobs = [
 @app.route("/")
 def hello():
     return render_template("home.html", jobs=jobs)
+
+@app.route("/api/jobs")
+def host_apis():
+    return jsonify(jobs)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
