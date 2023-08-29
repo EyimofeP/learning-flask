a8de2c92a281c7f2800c39c6e461a178ca0c52fb
Eyimofe <eyimofe36@gmail.com>
2023-08-29 12:09:23 +0100

    added indv. job page, added routing and aesthics

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def hello():
     jobs = load_jobs_from_db()
     return render_template("home.html", jobs=jobs)
 
-@app.route("/jobs/<id>")
+@app.route("/job/<id>")
 def job_description(id):
     job = load_job_from_db(id)
-    return jsonify(job)
+    return render_template("jobpage.html", job=job)
 
 @app.route("/api/jobs")
 def host_apis():
